d6597bf6854dba285a91de46a101d9cc4729668a
djulzz <julien.esposito@gmail.com>
2017-06-21 14:23:01 -0700

    Week 2 - Assignment Ch4 - Up to 4

---
+++ sql/week2/assignment_week_02_ch4.sql
@@ -0,0 +1,14 @@
+-- show databases;
+
+-- Q1
+-- select book_code, type, title from book;
+
+-- Q2
+-- select * from branch;
+
+-- Q3
+-- select publisher_name from publisher where ( city = 'Boston' );
+
+-- Q4
+-- select publisher_name, city from publisher where ( city <> 'New York' );
+-- select publisher_name, city from publisher where not ( city = 'New York' );
