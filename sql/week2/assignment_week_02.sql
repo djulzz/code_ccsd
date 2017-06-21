efc8a9c3a3608f5a813da9f72fb2b880ec1c3f7f
djulzz <julien.esposito@gmail.com>
2017-06-21 14:00:56 -0700

    Done with Week 2, Chapter 3 Assignment

---
+++ sql/week2/assignment_week_02.sql
@@ -0,0 +1,26 @@
+-- show databases;
+
+
+-- Week 2 - Assignment p.91
+-- Question 01
+-- create table SALES_BRANCH(
+-- 	BRANCH_NUM	INT primary key,
+-- 	BRANCH_NAME char(50),
+-- 	BRANCH_LOCATION varchar(50),
+-- 	NUM_EMPLOYEES INT
+-- );
+
+-- Question 02
+-- -----------
+-- insert into SALES_BRANCH values(
+-- 	5, 'Henry Town Plaza', '165 Plaza', 3
+-- );
+
+-- Next, we display our entry
+-- select * from SALES_BRANCH;
+
+-- Question 03
+-- -----------
+-- drop table SALES_BRANCH;
+
+-- Question 04 --> Done Last Week
