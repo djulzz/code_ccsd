02d5eb85e525d531d5744ae24727aeeed3dba870
djulzz <julien.esposito@gmail.com>
2017-06-27 16:21:22 -0700

    Finished Week 2
    Finished week 2, and introduced Chapter 5

--- sql/week2/assignment_week_02_ch4.sql
+++ sql/week2/assignment_week_02_ch4.sql
@@ -12,3 +12,19 @@
 -- Q4
 -- select publisher_name, city from publisher where ( city <> 'New York' );
 -- select publisher_name, city from publisher where not ( city = 'New York' );
+
+-- Q18
+-- select type, TRUNCATE( avg( price ), 2 ) as Average_Price from book where (paperback = 'Y') group by type;
+
+-- select type, avg( price ) as Average_Price from book where ( paperback = 'Y') group by type  having ( avg( price ) > 10 ) ;
+
+-- What are the title(s) and price(s) of the least expensive book(s) in the database?
+
+-- select title, price from book order by price desc;
+
+
+-- select price, title from book where (price = ( select max( price ) from book ));
+
+select B.book_code, A.author_num, title, author_first, author_last from
+book B, author A, wrote W where ( B.book_code = W.book_code ) and 
+( W.author_num = A.author_num );
