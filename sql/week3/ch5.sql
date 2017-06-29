f55603481e62762c2190776f62a8d7675724d5c0
djulzz <julien.esposito@gmail.com>
2017-06-29 15:56:04 -0700

    Henry Books Ch.5 Finished

--- sql/week3/ch5.sql
+++ sql/week3/ch5.sql
@@ -35,10 +35,62 @@
 -- in Q6 - To be continued
 
 -- Q8
-select b.book_code, title from wrote w, book b, inventory i
- where
- 	( b.book_code = w.book_code ) and
- 	( w.book_code = i.book_code ) and
- 	( b.book_code = i.book_code ) and
- 	( i.branch_num = 2 ) and
- 	( w.author_num = 20 );
+-- select b.book_code, title from wrote w, book b, inventory i
+--  where
+--  	( b.book_code = w.book_code ) and
+--  	( w.book_code = i.book_code ) and
+--  	( b.book_code = i.book_code ) and
+--  	( i.branch_num = 2 ) and
+--  	( w.author_num = 20 );
+
+-- Q9
+-- select f.book_code, s.book_code,
+--  f.title, s.title, f.price from book f, book s where
+--  (f.price = s.price ) and ( f.book_code > s.book_code )
+--   order by f.book_code asc;
+
+-- Q10
+-- select title, author_last, on_hand from book b, author a, inventory i,
+--  wrote w where
+--   ( b.book_code = w.book_code ) and
+--   ( a.author_num = w.author_num ) and
+--   ( i.book_code = b.book_code ) and
+--   ( i.book_code = w.book_code ) and
+--   ( i.branch_num = 4 );
+
+  -- Q11
+ --  select title, paperback, author_last, on_hand from book b, author a, inventory i,
+ -- wrote w where
+ --  ( b.book_code = w.book_code ) and
+ --  ( a.author_num = w.author_num ) and
+ --  ( i.book_code = b.book_code ) and
+ --  ( i.book_code = w.book_code ) and
+ --  ( i.branch_num = 4 ) and
+ --  ( b.paperback = 'Y');
+
+-- Q12
+ -- select book_code, title, price, city from book b, publisher p where 
+ --  ( b.publisher_code = p.publisher_code ) and 
+ --  ( ( p.city = 'Boston' ) or ( b.price > 10 ) );
+
+ -- Q13
+  -- select book_code, title, price, city from book b, publisher p where 
+  -- ( b.publisher_code = p.publisher_code ) and 
+  -- ( ( p.city = 'Boston' ) and ( b.price > 10 ) );
+
+  -- Q14
+  --   select book_code, title, price, city from book b, publisher p where 
+  -- ( b.publisher_code = p.publisher_code ) and 
+  -- ( ( p.city != 'Boston' ) and ( b.price > 10 ) );
+
+  -- Q15
+  -- select book_code, title, price from book 
+  -- where ( price > ( select max( price ) from book where type = 'MYS' ) );
+
+-- Q16
+  --   select book_code, title, price from book 
+  -- where ( price > ( select min( price ) from book where type = 'MYS' ) );
+
+ -- Q17
+-- select b.book_code, b.title, i.on_hand, i.branch_num from book b left join inventory i
+--  on( ( i.branch_num = 2 ) and ( b.book_code = i.book_code ) )order by b.book_code;
