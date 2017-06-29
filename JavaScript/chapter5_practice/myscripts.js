ca2c88067ab5c05da63078ca9e1ef195bc160790
djulzz <julien.esposito@gmail.com>
2017-06-29 12:48:41 -0700

    JavaScript - Playing with SELECT elements

---
+++ JavaScript/chapter5_practice/myscripts.js
@@ -0,0 +1,10 @@
+function updateSelection()
+{
+	var v1 = window.document.f1.selection.selectedIndex;
+	window.alert( v1 );
+}
+
+function InitSelection()
+{
+	window.document.f1.selection.selectedIndex = -1;
+}
