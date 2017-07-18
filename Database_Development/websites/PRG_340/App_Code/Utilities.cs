1ed39f381b0f9ee2c6cf45c30fe6556c70edef56
djulzz <julien.esposito@gmail.com>
2017-07-18 16:16:59 -0700

    Added Utility Methods, along with working student Entry.

---
+++ Database_Development/websites/PRG_340/App_Code/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+/// <summary>
+/// Summary description for Utilities
+/// </summary>
+public class Utilities
+{
+    public Utilities()
+    {
+
+    }
+
+
+    public static String Stringify( String input )
+    {
+        return "'" + input + "'";
+    }
+}
--- Database_Development/websites/PRG_340/ManageStudents.aspx.cs
+++ Database_Development/websites/PRG_340/ManageStudents.aspx.cs
@@ -25,19 +25,56 @@ public partial class ManageStudents : System.Web.UI.Page
         }
     }
 
+    protected String s( String input )
+    {
+        return "'" + input + "'";
+    }
+    
     protected void add_Student(object sender, EventArgs e)
     {
         string id               = s_id.Text;
-        string firstName        = s_firstName.Text;
-        string lastName         = s_lastName.Text;
+        string firstName        = Utilities.Stringify( s_firstName.Text );
+        string lastName         = Utilities.Stringify( s_lastName.Text );
         string gpa              = s_gpa.Text;
-        string shift            = s_shift.Text;
-        string email            = s_email.Text;
-        string phoneNumber      = s_phoneNumber.Text;
-        string programCode      = s_programCode.Text;
+        string shift            = Utilities.Stringify( s_shift.Text);
+        string email            = Utilities.Stringify( s_email.Text);
+        string phoneNumber      = Utilities.Stringify( s_phoneNumber.Text);
+        string programCode      = Utilities.Stringify( s_programCode.Text);
 
         int cvt_id = Convert.ToInt32(id);
         double cvt_gpa = Convert.ToDouble(gpa);
+
+        int return_value = 0;
+
+        String sep = " , ";
+        String Sql_non_query_string = @"insert into student values(" +
+            cvt_id + sep +
+            firstName + sep +
+            lastName + sep +
+            cvt_gpa + sep +
+            shift + sep +
+            email + sep +
+            phoneNumber + sep +
+            programCode + ")";
+
+        m_cnx = (MySqlConnection)Session[ConnexionKey];
+        MySqlCommand cmd = new MySqlCommand(Sql_non_query_string, m_cnx );
+
+        try
+        {
+            return_value = cmd.ExecuteNonQuery();
+        } catch( MySqlException ex )
+        {
+            Response.Write("Error while attempting to insert a student in the database.\r\n");
+            Response.Write("Dev. Insight = Exception --> " + ex.Message);
+        }
+        finally
+        {
+            if( return_value == 1 )
+            {
+                MessageBox.Show(this, "Student (" + firstName + "," + lastName + ") successfully added");
+            }
+        }
 
     }
 }
